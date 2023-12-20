1604ff1b2600f6079ff93db7b5b1e8c24c3b3197
kushagra Shukla <kushalshukla110@gmail.com>
2023-12-20 12:22:20 -0500

    cammera wowo

--- pages/constants/movie.tsx
+++ pages/constants/movie.tsx
@@ -1,1 +1,12 @@
-export const baseUrl = 'https://image.tmdb.org/t/p/original/'
+// pages/constants/movie.js
+
+import React from "react";
+
+const MoviePage = () => {
+  // Your component logic here
+  return <div>Movie Page</div>;
+};
+
+export default MoviePage;
+
+export const baseUrl = 'https://image.tmdb.org/t/p/original/';
